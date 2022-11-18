e98d665954a024cbb54da41a9b5a6ec01d08ae84
pengzhanbo <volodymyr@foxmail.com>
2022-11-18 18:29:49 +0800

    feat: overload defineMock

--- src/define.ts
+++ src/define.ts
@@ -1,18 +1,26 @@
 import type { MockOptions, MockOptionsItem } from './types'
 
 /**
+ * mock config helper
+ *
  * mock配置帮助函数
+ *
  * @param config
  * @example
  * ```ts
  * export default defineMock({
  *   url: '/api/example',
+ *   method: ['GET', 'POST'],
  *   body: { a: 1 },
  * })
  * ```
  */
-export const defineMock = (
-  config: MockOptionsItem | MockOptions
-): MockOptionsItem | MockOptions => config
+export function defineMock(config: MockOptionsItem): MockOptionsItem
+export function defineMock(config: MockOptions): MockOptions
+export function defineMock(
+  config: MockOptions | MockOptionsItem
+): MockOptions | MockOptionsItem {
+  return config
+}
 
 export default defineMock
