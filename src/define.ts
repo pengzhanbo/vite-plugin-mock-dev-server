04e8ee818683a3eb8f6c21970629aeb4f0a93e32
pengzhanbo <volodymyr@foxmail.com>
2023-03-22 22:44:16 +0800

    feat: add `createDefineMock` method

--- src/define.ts
+++ src/define.ts
@@ -1,4 +1,5 @@
 import type { MockOptions, MockOptionsItem } from './types'
+import { isArray } from './utils'
 
 /**
  * mock config helper
@@ -23,4 +24,23 @@ export function defineMock(
   return config
 }
 
-export default defineMock
+/**
+ * 返回一个自定义的 defineMock 函数，用于支持对 mock config 的预处理。
+ *
+ * Return a custom defineMock function to support preprocessing of mock config.
+ *
+ * @param transformer preprocessing function
+ */
+export function createDefineMock(
+  transformer: (mock: MockOptionsItem) => MockOptionsItem | void,
+): typeof defineMock {
+  const define = (config: MockOptions | MockOptionsItem) => {
+    if (isArray(config)) {
+      config = config.map((item) => transformer(item) || item)
+    } else {
+      config = transformer(config) || config
+    }
+    return config
+  }
+  return define as typeof defineMock
+}
