d42ff20d7ae138feb7ef04059f036685ea26801f
pengzhanbo <volodymyr@foxmail.com>
2023-04-26 03:52:17 +0800

    docs: Improve the example

--- example/src/request.ts
+++ example/src/request.ts
@@ -3,13 +3,33 @@ const jsonHeader = {
 }
 
 export async function get(url: string) {
-  return await fetch(url, { method: 'GET', headers: jsonHeader })
+  const response = await fetch(url, { method: 'GET', headers: jsonHeader })
+  await renderResponse(url, 'GET', response)
 }
 
 export async function post(url: string, body?: Record<string, any>) {
-  return await fetch(url, {
+  const response = await fetch(url, {
     method: 'POST',
     body: body ? JSON.stringify(body) : undefined,
     headers: jsonHeader,
   })
+  await renderResponse(url, 'POST', response)
+}
+
+async function renderResponse(url: string, method: string, response: Response) {
+  const container = document.createElement('div')
+  container.classList.add('container')
+  let content = `<p><b>Request URL:</b><span>${url}</span></p>
+  <p><b>Request Method:</b><span>${method}</span></p>
+  <p><b>Request Status:</b>${response.status} ${response.statusText}</p>`
+  if (response.ok) {
+    let result = {}
+    try {
+      result = await response.json()
+    } catch {}
+    const str = JSON.stringify(result, null, 2)
+    content += `<div><p><b>Response Body: </b></p><pre><code>${str}</code></pre></div>`
+  }
+  container.innerHTML = content
+  document.body.appendChild(container)
 }
