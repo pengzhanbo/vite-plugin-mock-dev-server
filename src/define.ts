b0b835f006636d74c58af6c84da91d1b44033649
pengzhanbo <volodymyr@foxmail.com>
2023-05-17 13:38:26 +0800

    chore: add define comments

--- src/define.ts
+++ src/define.ts
@@ -2,17 +2,31 @@ import type { MockHttpItem, MockOptions, MockWebsocketItem } from './types'
 import { isArray } from './utils'
 
 /**
- * mock config helper
+ * mock config Type helper
  *
- * mock配置辅助函数
- *
+ * mock配置 类型帮助函数
  * @param config
  * @example
+ * Mock Http Request
  * ```ts
  * export default defineMock({
  *   url: '/api/example',
  *   method: ['GET', 'POST'],
  *   body: { a: 1 },
+ * })
+ * ```
+ * @example
+ * Mock WebSocket
+ * ```ts
+ * export default defineMock({
+ *   url: '/socket.io',
+ *   ws: true,
+ *   setup(wss) {
+ *     wss.on('connection', (ws) => {
+ *       ws.on('message', (rawData) => console.log(rawData))
+ *       ws.send('data')
+ *     })
+ *   },
  * })
  * ```
  */
@@ -26,11 +40,20 @@ export function defineMock(
 }
 
 /**
- * 返回一个自定义的 defineMock 函数，用于支持对 mock config 的预处理。
- *
  * Return a custom defineMock function to support preprocessing of mock config.
  *
+ * 返回一个自定义的 defineMock 函数，用于支持对 mock config 的预处理。
  * @param transformer preprocessing function
+ * @example
+ * ```ts
+ * const definePostMock = createDefineMock((mock) => {
+ *   mock.url = '/api/post/' + mock.url
+ * })
+ * export default definePostMock({
+ *   url: 'list',
+ *   body: [{ title: '1' }, { title: '2' }],
+ * })
+ * ```
  */
 export function createDefineMock(
   transformer: (
