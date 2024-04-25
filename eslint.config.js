49cfc627af7518c9c9e601b00cb297bc882abddc
pengzhanbo <volodymyr@foxmail.com>
2024-04-25 20:36:33 +0800

    chore: update deps to latest

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
 import config from '@pengzhanbo/eslint-config'
 
-export default config({}, {
+export default config({
+  vue: false,
+}, {
   files: ['**/*.md/*.ts'],
   rules: {
     'node/prefer-global/buffer': 'off',
