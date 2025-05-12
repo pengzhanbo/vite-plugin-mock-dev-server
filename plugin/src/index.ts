8d915bd05c23abf449eedc387f46e1c2227b6eba
pengzhanbo <volodymyr@foxmail.com>
2025-05-12 18:55:03 +0800

    feat: deprecate default export and encourage named export usage

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -1,3 +1,6 @@
+import type { Plugin } from 'vite'
+import type { MockServerPluginOptions } from './types'
+import pc from 'picocolors'
 import { mockDevServerPlugin } from './plugin'
 
 export * from './helper'
@@ -13,3 +16,14 @@ export type {
 } from './types'
 
 export { mockDevServerPlugin }
+
+/**
+ * @deprecated use named export instead
+ */
+export default function mockDevServerPluginWithDefaultExportWasDeprecated(
+  options: MockServerPluginOptions = {},
+): Plugin[] {
+  console.warn(`${pc.yellow('[vite-plugin-mock-dev-server]')} ${pc.yellow(pc.bold('WARNING:'))} The plugin default export is ${pc.bold('deprecated')}, it will be removed in next major version, use ${pc.bold('named export')} instead:\n\n    ${pc.green('import { mockDevServerPlugin } from "vite-plugin-mock-dev-server"')}\n`)
+
+  return mockDevServerPlugin(options)
+}
