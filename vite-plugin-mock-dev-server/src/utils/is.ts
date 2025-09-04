219db37353f93e0c16c1d02315af1d45e73c7442
pengzhanbo <volodymyr@foxmail.com>
2025-09-04 12:24:50 +0800

    fix: `isPackageExists` checks for the package incorrectly, close #125

--- vite-plugin-mock-dev-server/src/utils/is.ts
+++ vite-plugin-mock-dev-server/src/utils/is.ts
@@ -1,4 +1,5 @@
 import type { Readable, Stream } from 'node:stream'
+import { hasOwn } from '@pengzhanbo/utils'
 
 export function isStream(stream: unknown): stream is Stream {
   return stream !== null
@@ -11,4 +12,26 @@ export function isReadableStream(stream: unknown): stream is Readable {
     && (stream as any).readable !== false
     && typeof (stream as any)._read === 'function'
     && typeof (stream as any)._readableState === 'object'
+}
+
+const PACKAGE_CACHE: Record<string, boolean> = {}
+
+export async function isPackageExists(mod: string): Promise<boolean> {
+  if (hasOwn(PACKAGE_CACHE, mod)) {
+    return PACKAGE_CACHE[mod]
+  }
+  try {
+    // @ts-expect-error fallback for node
+    if (import.meta.resolve) {
+      PACKAGE_CACHE[mod] = !!import.meta.resolve(mod)
+    }
+    else {
+      await import(mod)
+      PACKAGE_CACHE[mod] = true
+    }
+    return PACKAGE_CACHE[mod]
+  }
+  catch {}
+  PACKAGE_CACHE[mod] = false
+  return false
 }
