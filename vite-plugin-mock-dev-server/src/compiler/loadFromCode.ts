f1d69252ae8a95e2b45dd4ed83daa8f49da81a72
pengzhanbo <volodymyr@foxmail.com>
2026-05-11 15:46:35 +0800

    feat: ensure temp files are cleared

--- vite-plugin-mock-dev-server/src/compiler/loadFromCode.ts
+++ vite-plugin-mock-dev-server/src/compiler/loadFromCode.ts
@@ -1,8 +1,9 @@
-import { createHash } from 'node:crypto'
-import { promises as fsp } from 'node:fs'
+import fs, { promises as fsp } from 'node:fs'
 import path from 'node:path'
+import process from 'node:process'
 import { pathToFileURL } from 'node:url'
 import { attemptAsync } from '@pengzhanbo/utils'
+import { getHash } from '../utils'
 
 interface LoadFromCodeOptions {
   filepath: string
@@ -10,6 +11,14 @@ interface LoadFromCodeOptions {
   isESM: boolean
   cwd: string
 }
+
+/**
+ * 在一些 IDE 中，如果有使用到 vite 相关上下游的扩展，可能会加载执行 vite 配置，
+ * 导致插件的非预期的情况下被执行。
+ * 这可能会导致插件在错误的环境下被调用，生成临时文件后没有及时删除。
+ * 因此，在进程退出时，需要清理临时文件，确保没有遗留的临时文件。
+ */
+const TEMP_FILES = new Set<string>()
 
 export async function loadFromCode<T = any>({
   filepath,
@@ -21,8 +30,9 @@ export async function loadFromCode<T = any>({
   const ext = isESM ? '.mjs' : '.cjs'
   const filepathTmp = `${filepath}.${getHash(code)}${ext}`
   await fsp.writeFile(filepathTmp, code, 'utf8')
+  TEMP_FILES.add(filepathTmp)
   const [, mod] = await attemptAsync(importDefault, String(pathToFileURL(filepathTmp)))
-  await attemptAsync(fsp.unlink, filepathTmp)
+  await attemptAsync(unlink, filepathTmp)
   return mod
 }
 
@@ -31,6 +41,17 @@ async function importDefault(filepath: string): Promise<any> {
   return mod.default || mod
 }
 
-function getHash(str: string): string {
-  return createHash('md5').update(str).digest('hex')
+async function unlink(filepath: string) {
+  await fsp.unlink(filepath)
+  TEMP_FILES.delete(filepath)
 }
+
+async function cleanupTempFiles() {
+  for (const filepath of TEMP_FILES) {
+    if (fs.existsSync(filepath))
+      await attemptAsync(fsp.unlink, filepath)
+  }
+}
+
+// 在进程退出时，清理临时文件
+process.on('exit', cleanupTempFiles)
